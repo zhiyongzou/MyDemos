4768e8c5c9880493dbcd687a14a08cf8d40ed5b9
zzyong <scauzouzhiyong@163.com>
2020-02-25 16:07:45 +0800

    add iread

---
+++ iRead/iRead/Main/IRNavigationController.swift
@@ -0,0 +1,30 @@
+//
+//  IRNavigationController.swift
+//  iRead
+//
+//  Created by zzyong on 2020/2/25.
+//  Copyright © 2020 zzyong. All rights reserved.
+//
+
+import UIKit
+
+class IRNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iRead/iRead/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iRead
+//
+//  Created by zzyong on 2020/2/25.
+//  Copyright © 2020 zzyong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
