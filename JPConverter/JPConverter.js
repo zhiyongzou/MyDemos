dffc99723fb7ef27649025c630beaa8cf8613abd
zzyong <scauzouzhiyong@163.com>
2020-04-05 00:26:32 +0800

    update JPConverter

--- JPConverter/JPConverter.js
+++ JPConverter/JPConverter.js
@@ -6,4 +6,14 @@
 //  Copyright © 2020 zzyong. All rights reserved.
 //
 
+function tabKeyClick(obj) {
+    if (event.keyCode == 9)
+    {
+       obj.value = obj.value + "    ";
+       event.returnValue = false;
+    }
+  }
 
+function didConverterButtonClick() {
+    console.log("didConverterButtonClick");
+}
