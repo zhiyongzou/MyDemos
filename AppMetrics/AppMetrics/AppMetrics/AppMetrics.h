ed497d9613cb910e2693f085828df5ba217ff1d9
zzyong <zouzhiyong@corp.netease.com>
2020-05-13 15:12:53 +0800

    AppMetrics

---
+++ AppMetrics/AppMetrics/AppMetrics/AppMetrics.h
@@ -0,0 +1,17 @@
+//
+//  AppMetrics.h
+//  AppMetrics
+//
+//  Created by zzyong on 2020/5/13.
+//  Copyright © 2020 zzyong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AppMetrics : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AppMetrics/AppMetrics/AppMetrics/AppMetrics.m
@@ -0,0 +1,13 @@
+//
+//  AppMetrics.m
+//  AppMetrics
+//
+//  Created by zzyong on 2020/5/13.
+//  Copyright © 2020 zzyong. All rights reserved.
+//
+
+#import "AppMetrics.h"
+
+@implementation AppMetrics
+
+@end
