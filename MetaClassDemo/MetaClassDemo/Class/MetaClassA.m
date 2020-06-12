b73afc4fe65e591a275e1bfb83893e328790d47f
zzyong <zouzhiyong@corp.netease.com>
2020-06-12 19:57:01 +0800

    1

---
+++ MetaClassDemo/MetaClassDemo/Class/MetaClassA.m
@@ -0,0 +1,13 @@
+//
+//  MetaClassA.m
+//  MetaClassDemo
+//
+//  Created by zzyong on 2020/6/11.
+//  Copyright © 2020 zzyong. All rights reserved.
+//
+
+#import "MetaClassA.h"
+
+@implementation MetaClassA
+
+@end
---
+++ MetaClassDemo/MetaClassDemo/Class/MetaClassB.m
@@ -0,0 +1,13 @@
+//
+//  MetaClassB.m
+//  MetaClassDemo
+//
+//  Created by zzyong on 2020/6/11.
+//  Copyright © 2020 zzyong. All rights reserved.
+//
+
+#import "MetaClassB.h"
+
+@implementation MetaClassB
+
+@end
